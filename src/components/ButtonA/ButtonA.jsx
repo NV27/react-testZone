f61144207c3bb3c1ebac436bd622a87463e5fd89
Louis <louisprotano@hotmail.com>
2024-04-25 18:09:25 +0100

    win state for infinite buttons

--- src/components/ButtonA/ButtonA.jsx
+++ src/components/ButtonA/ButtonA.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react';
 import './style.css'
 
-const ButtonA = ({defaultClickCount}) => {
+const ButtonA = ({defaultClickCount, iWin}) => {
 
     const [clickCount, setClickCount] = useState(defaultClickCount);
+    
+
 
     const handleClick = () => {
         setClickCount(clickCount*clickCount);
         console.log("clicked: " + clickCount);
+
+        if (clickCount == "Infinity"){
+            iWin();
+            console.log("I win")
+        }
+
     }
+        
 
     return(
         <>
--- src/components/InfiniteSquaring/InfiniteSquaring.jsx
+++ src/components/InfiniteSquaring/InfiniteSquaring.jsx
@@ -3,15 +3,24 @@ import ButtonA from '../ButtonA/ButtonA';
 
 const InfiniteSquaring = () => {
 
-    
+    const [winFunction, setWinFunction] = useState(false);
+    const [iWon, setiWon] = useState(false);
+
+    let winText = false;
+
+    const iWin = () => {
+        setiWon(true);
+    }
 
     return(
+        <>
         <div>
-            <ButtonA defaultClickCount={2}/>
-            <ButtonA defaultClickCount={3}/>
+            <ButtonA defaultClickCount={2} iWin={iWin}/>
+            <ButtonA defaultClickCount={3} iWin={iWin}/>
         </div>
 
-
+        {iWon && <h2>You Win"</h2>}
+        </>
     )
 }
 
